b77777d4dcb2e8ded596254b4d95b0f8df2454fa
002-umulkher <umulkherjabir@gmail.com>
2026-03-10 12:25:31 +0300

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    const buttons = card.querySelectorAll(".navigation-button");
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        card.classList.toggle("change");
+      });
+    });
+  });
+});
